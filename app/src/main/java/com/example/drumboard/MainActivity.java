46416f3f4efdd0a358d1ff736501e8d9bb560579
201004641 <p0ppyc0ck283@gmail.com>
2023-05-25 11:50:57 -0700

    one button prolly works

--- app/src/main/java/com/example/drumboard/MainActivity.java
+++ app/src/main/java/com/example/drumboard/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.drumboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton btnBass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnBass = findViewById(R.id.btn_bass_drum);
+        private void playBass() {
+            MediaPlayer mp = MediaPlayer.create(this, R.raw.sample_bass_hit);
+            mp.start();
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.stop();
+                    mp.release();
+                    mp=null;
+                }
+            });
+        }
+
+
+        btnBass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBass();
+            }
+        });
     }
 }
